6d949df046282cfe071a4bfadd417bede5c188ff
Yuki200K <sinhalacartoon201@gmail.com>
2025-04-08 20:01:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=41REUTKQ#uDoc_qdd2KlxkPDftP8Gfptl1FuINR0zGc0C-HsWAlg",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
